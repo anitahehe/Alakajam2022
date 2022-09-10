b94b8b7e36d390be9b4e1b4f1f10478d769652fc
John <John.wang@ontariotechu.net>
2022-09-10 14:15:18 -0400

    ai nav

--- Alakajam2022/Assets/Scripts/PirateAIController.cs
+++ Alakajam2022/Assets/Scripts/PirateAIController.cs
@@ -7,21 +7,102 @@ using UnityEngine;
  * 2. Player must be in danger zone
  */
 
-
 public class PirateAIController : BoatController
 {
-    public Transform target;
+    public Transform playerTarget;
+    public DangerZone dangerZone;
+    public Transform[] patrolPointArray;
+    public Queue<Transform> patrolPoints;
 
-    public float sightRange;
-    public bool sightCheck = false;
+    public float angularTrackingAccuracy = 15;
 
-    // Start is called before the first frame update
-    void Start()
+    public Transform currentPatrolPointDebug;
+
+    public float sightRange = 20.0f;
+    public bool inChase = false;
+
+    public void Start()
     {
+        patrolPoints = new Queue<Transform>();
+        foreach (Transform patrolPoint in patrolPointArray)
+        {
+            patrolPoints.Enqueue(patrolPoint);
+        }
+        currentPatrolPointDebug = patrolPoints.Peek();
+    }
+
+    public void CalculateNavigation(Transform target)
+    {
+        Vector2 dir = (target.position - this.transform.position).normalized;
+
+        float xdiff = Vector2.Angle(this.transform.right, dir);
+        float ydiff = Vector2.Angle(this.transform.up, dir);
+
+        if (ydiff < 90)
+        {
+            forward = true;
+
+            if (xdiff > 90 + angularTrackingAccuracy)
+            {
+                right = false;
+                left = true;
+            }
+            else if (xdiff < 90 - angularTrackingAccuracy)
+            {
+                right = true;
+                left = false;
+            }
+            else
+            {
+                right = false;
+                left = false;
+            }
+        }
+        else 
+        {
+            forward = false;
+            if (xdiff > 90)
+            {
+                right = false;
+                left = true;
+            }
+            else
+            {
+                right = true;
+                left = false;
+            }
+        }
+    }
+
+    public Transform GetNextPatrolPoint()
+    {
+        Transform nextTransform = patrolPoints.Dequeue();
+        patrolPoints.Enqueue(nextTransform);
+        currentPatrolPointDebug = patrolPoints.Peek();
+        return patrolPoints.Peek();
     }
 
     private void Update() 
-    { 
-       
+    {
+        // Check for sightRange
+        if (inChase)
+        {
+            CalculateNavigation(playerTarget);
+        }
+        else
+        {
+            CalculateNavigation(patrolPoints.Peek());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PatrolPoint")
+        {
+            if(collision.transform == patrolPoints.Peek())
+            {
+                GetNextPatrolPoint();
+            }
+        }
     }
 }
