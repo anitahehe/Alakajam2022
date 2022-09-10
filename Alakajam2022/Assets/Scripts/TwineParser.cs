842391813a48e45ecb22c2fdde624d45d76241e4
John <John.wang@ontariotechu.net>
2022-09-10 11:38:43 -0400

    Added twine parser

---
+++ Alakajam2022/Assets/Scripts/TwineParser.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * To Use:
+ * 1. Get Story using GetStoryByIndex(int index) 
+ * 2. Get First Passage using StoryInfo.GetStartingPassage()
+ * 
+ * 3. Check for Dialogie branches using Passage.HasMultipleBranches(), Passage.NumOfBranches() and Passage.IsTerminalNode()
+ * 4. Output dialogue using Passage.text, Passage.name
+ * 5. Output branches using Link.name
+ * 6. Get Next Dialogue Id using Passage.GetNextPassageId(int branchIndex = 0)
+ * 7. Repeat step 3
+ */
+
+
+
+public class TwineParser : MonoBehaviour
+{
+    [Header("###########Add in Twine stories here:###########")]
+    public TextAsset[] stories;
+
+    public List<StoryInfo> storyInfo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        storyInfo = new List<StoryInfo>();
+        foreach(TextAsset story in stories)
+        {
+            StoryInfo info = StoryInfo.CreateFromJSON(story.ToString());
+            storyInfo.Add(info);
+        }
+    }
+
+    public StoryInfo GetStoryByIndex(int index)
+    {
+        StoryInfo returnStory = null;
+        if (index < storyInfo.Count)
+        {
+            returnStory = storyInfo[index];
+        }
+        else
+        {
+            Debug.LogError("Story index out of scope");
+        }
+        return returnStory;
+    }
+}
