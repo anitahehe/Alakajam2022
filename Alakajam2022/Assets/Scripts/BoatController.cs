a1b03e63722d892954696e240698c198287d72d4
John <John.wang@ontariotechu.net>
2022-09-10 12:04:13 -0400

    updated boat controls

--- Alakajam2022/Assets/Scripts/BoatController.cs
+++ Alakajam2022/Assets/Scripts/BoatController.cs
@@ -12,30 +12,73 @@ public class BoatController : MonoBehaviour
     
     private Rigidbody rb;
 
+    protected bool forward = false;
+    protected bool backwards = false;
+    protected bool left = false;
+    protected bool right = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            forward = true;
+            backwards = false;
+        }
+        else if (Input.GetKey("s"))
+        {
+            backwards = true;
+            forward = false;
+        }
+        else
+        {
+            backwards = false;
+            forward = false;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            left = true;
+            right = false;
+        }
+        else if (Input.GetKey("d"))
+        {
+            left = false;
+            right = true;
+        }
+        else 
+        {
+            left = false;
+            right = false;
+        }
+    }
+
     void FixedUpdate()
     {
         // Apply forward/reverse thrust and hand-applied deceleration.
-        if (Input.GetKey("w"))
+        if (forward)
         {
             rb.AddForce(transform.up * forwardThrust);
-        } else if (Input.GetKey("s"))
+        } 
+        if (backwards)
         {
             rb.AddForce(-transform.up * reverseThrust);
-        } else
+        } 
+        if(!forward && !backwards)
         {
             rb.velocity -= deceleration * rb.velocity;
         }
         // Apply torque based turning.
         Quaternion lastRotation = transform.rotation;
-        if (Input.GetKey("a"))
+        if ((left && forward) || (right && backwards))
         {
             rb.AddRelativeTorque(-transform.forward * torque);
-        } else if (Input.GetKey("d"))
+        } 
+        if ((right && forward) || (left && backwards))
         {
             rb.AddRelativeTorque(transform.forward * torque);
         }
