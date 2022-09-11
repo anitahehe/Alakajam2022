87950e9c25ca90952e6639e5cd3f69073e5ed60b
Adriana Matic <39062146+balidoria@users.noreply.github.com>
2022-09-10 23:29:44 -0600

    Sound Pass
    Fixed fade coroutine, added in test music, added in trigger on dialog start so we can put in music

--- Alakajam2022/Assets/Scripts/TriggerDialoog.cs
+++ Alakajam2022/Assets/Scripts/TriggerDialoog.cs
@@ -7,11 +7,14 @@ public class TriggerDialoog : MonoBehaviour
     public DialogUI dialogUIScript;
 
     public int storyNumber; 
+    public AudioSource audioObject;
+    public AudioClip characterMusic;
 
    void OnTriggerEnter2D(Collider2D collision){
        if(collision.gameObject.tag == "Player"){
            Debug.Log("Story is triggered");
            dialogUIScript.InitializeStory(storyNumber);
+           audioObject.PlayOneShot(characterMusic);
 
 
 
