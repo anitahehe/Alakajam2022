6d2a1d2e11c298ca51511979800cb3b387661477
Adriana Matic <39062146+balidoria@users.noreply.github.com>
2022-09-11 11:05:50 -0600

    Sound
    Music slotted in
    Sound effects and delay

--- Alakajam2022/Assets/Scripts/PaddlingSound.cs
+++ Alakajam2022/Assets/Scripts/PaddlingSound.cs
@@ -6,28 +6,31 @@ public class PaddlingSound : MonoBehaviour
 {
     public AudioSource audioObject;
     public float FadeTime;
+    bool disableSound;
     // Start is called before the first frame update
     
-  
-    void Start()
-    {
-        
-    }
+    public float delay = 1;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("w")){
             audioObject.Play();
-            audioObject.volume = 0.5f;
+            audioObject.volume = 0.25f;
             StopAllCoroutines();
-
-        }
-        if(Input.GetKeyUp("w")){
-            StartCoroutine (FadeOut (audioObject, FadeTime));
             
 
         }
+        if(Input.GetKeyUp("w")){
+            disableSound = true;
+          
+          StartCoroutine (FadeOut (audioObject, FadeTime));
+
+        }
+        if(disableSound){
+            DisableSound();
+        }
+        
     }
      public static IEnumerator FadeOut (AudioSource audioObject, float FadeTime) {
         float startVolume = audioObject.volume;
@@ -40,5 +43,18 @@ public class PaddlingSound : MonoBehaviour
  
         audioObject.Stop ();
         audioObject.volume = startVolume;
+    }
+    void DisableSound(){
+        
+          delay = (delay-1 * Time.deltaTime);
+            if(delay <= 0){
+                Debug.Log("Stop Sounds");
+                //fallback since the couroutine gets stuck in dialog
+                audioObject.Stop ();
+                delay = 1;
+                disableSound = false;
+                
+            }
+
     }
 }
