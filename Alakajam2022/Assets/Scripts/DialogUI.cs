1d9275ed9f1a4d990fb106be41558ffdac9a15d6
klaytronic <klaytoncheukgamedev@gmail.com>
2022-09-11 00:46:53 -0400

    Giving DialogUI an Updo
    - character support
    - multi-line passage support
    - it is assumed that passage with no links is intended to end the dialog.
    - fixed lil parsing error causing the parser to not snip up the text list
    - dialog initialization now stops time; anything that needs to run/animate during dialog should use UnscaledDeltaTime

--- Alakajam2022/Assets/Scripts/DialogUI.cs
+++ Alakajam2022/Assets/Scripts/DialogUI.cs
@@ -11,14 +11,16 @@ public class DialogUI : MonoBehaviour
     [TabGroup("References")] public GameObject UI;
     [TabGroup("References")] public TMP_Text lineText;
     [TabGroup("References")] public TMP_Text nameText;
-    [TabGroup("References")] public GameObject continueIndicator;
+    [TabGroup("References")] public Image continueIndicator;
     [TabGroup("References")] public Transform optionsParent;
     [TabGroup("References")] public GameObject optionButtonPrefab;
     [TabGroup("References")] public Image portrait;
-
+    [TabGroup("References")] public Image textbox;
 
     [TabGroup("UX")] public float textSpeed;
     [TabGroup("UX")] public KeyCode skipKey;
+    [TabGroup("UX")] public Color fadedColor;
+    [TabGroup("UX")] public float fadeDuration;
 
     [TabGroup("CharData")] public Character[] characters;
     Character currentCharacter;
@@ -33,11 +35,12 @@ public class DialogUI : MonoBehaviour
     bool textButtonPressed;
     bool optionSelected;
 
+    IEnumerator ColorRoutine;
+
     TwineParser parser;
     public void Start()
     {
         parser = GetComponent<TwineParser>();
-
         // DEBUGGING, REMOVE LATER
         //InitializeStory(0);
         
@@ -51,6 +54,7 @@ public class DialogUI : MonoBehaviour
         UI.SetActive(true);
         currentPassageID = 0;
         RunPassage(currentStory.GetStartingPassage());
+        Time.timeScale = 0;
     }
 
     public void Deactivate()
@@ -72,92 +76,98 @@ public class DialogUI : MonoBehaviour
         }
     }
 
+    
+
     IEnumerator DoRunPassage(Passage passage)
     {
-        string[] text = passage.text.Split(" ", 2);
+        textButtonPressed = true;
 
-        // test if we need a new character to be displayed
-        bool newChar = currentCharacter == null;
-        if (!newChar)
-            newChar = currentCharacter.nameID == text[0];
-
-        if (newChar)
+        while (passage.HasNext())
         {
-            foreach (Character character in characters)
-            {
-                if (text[0] == character.nameID)
-                {
-                    ChangeCharacter(character);
-                    break;
-                }
-            }
-        }
-        
-        lineText.text = text[1];
-        textButtonPressed = false;
-        continueIndicator.SetActive(false);
-        foreach (Transform child in optionsParent)
-        {
-            Destroy(child.gameObject);
-        }
+            continueIndicator.gameObject.SetActive(true);
 
-        // animate 
-        int counter = 0;
-        if (textSpeed > 0.0f)
-        {
-            foreach (char c in lineText.text)
-            {
-                // make characters visible slowly
-                counter++;
-                lineText.maxVisibleCharacters = counter;
-
-                if (textButtonPressed)
-                {
-                    // We've requested a skip of the entire line.
-                    // Display all of the text immediately.
-                    lineText.maxVisibleCharacters = lineText.text.Length;
-                    break;
-                }
-
-                yield return new WaitForSeconds(textSpeed);
-            }
-        }
-
-        OnLineComplete.Invoke();
-
-        // check if we branch. Calling the built in checkers throws a nullref if there are no links, so we do this to save time.
-        bool noBranch = passage.links == null;
-        if (passage.links != null)
-            noBranch = passage.links.Length <= 1;
-
-        if (noBranch)
-        {
-            textButtonPressed = false;
-
-            continueIndicator.SetActive(true);
             // wait for a skip key to move to next line
             while (textButtonPressed == false)
                 yield return null;
 
-            if (passage.links == null)
-                Deactivate();
-            else if (passage.links.Length == 0)
-                Deactivate();
-            else
-                RunPassage(currentStory.GetPassageByID(passage.links[0].pid));
+            string[] text = passage.GetNext().Split("\n", 2);
+
+            // test if we need a new character to be displayed
+            bool newChar = currentCharacter == null;
+            if (!newChar)
+                newChar = currentCharacter.nameID != text[0];
+
+            if (newChar)
+            {
+                foreach (Character character in characters)
+                {
+                    if (text[0] == character.nameID)
+                    {
+                        ChangeCharacter(character);
+                        continue;
+                    }
+                }
+            }
+
+            lineText.text = text[text.Length - 1];
+            textButtonPressed = false;
+            continueIndicator.gameObject.SetActive(false);
+            foreach (Transform child in optionsParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // animate 
+            int counter = 0;
+            if (textSpeed > 0.0f)
+            {
+                foreach (char c in lineText.text)
+                {
+                    // make characters visible slowly
+                    counter++;
+                    lineText.maxVisibleCharacters = counter;
+
+                    if (textButtonPressed)
+                    {
+                        // We've requested a skip of the entire line.
+                        // Display all of the text immediately.
+                        lineText.maxVisibleCharacters = lineText.text.Length;
+                        break;
+                    }
+
+                    float timer = 0;
+                    while (timer < textSpeed)
+                    {
+                        timer += Time.unscaledDeltaTime;
+                        yield return null;
+                    }
+                }
+            }
+
+            OnLineComplete.Invoke();
+            textButtonPressed = false;
         }
-        else 
+
+        if (passage.links == null)
+        {
+            OnDialogComplete.Invoke();
+            Deactivate();
+        }
+        else
         {
             optionSelected = false;
-            // display all options
-            foreach(Link link in passage.links)
+            foreach (Link link in passage.links)
             {
                 Button button = Instantiate(optionButtonPrefab, optionsParent).GetComponent<Button>();
                 button.GetComponentInChildren<TMP_Text>().text = link.name;
+                button.GetComponent<Image>().color = currentCharacter.setColor;
                 button.onClick.AddListener(() => RunOption(link.pid));
             }
+
             while (optionSelected == false)
                 yield return null;
+
+            yield break;
         }
     }
 
@@ -170,9 +180,54 @@ public class DialogUI : MonoBehaviour
 
     public void ChangeCharacter(Character character)
     {
-        portrait.sprite = character.portrait;
+        if (portrait.sprite != null && character.portrait == null)
+        {
+            if (ColorRoutine != null)
+            {
+                StopCoroutine(ColorRoutine);
+                ColorRoutine = null;
+            }
+            ColorRoutine = PortraitColorChange(fadedColor, fadeDuration);
+            StartCoroutine(ColorRoutine);
+        }
+        else if (portrait.sprite == null && character.portrait == null)
+        {
+            portrait.color = Color.clear;
+        }
+        else if (portrait.color != Color.white && character.portrait != null)
+        {
+            if (ColorRoutine != null)
+            {
+                StopCoroutine(ColorRoutine);
+                ColorRoutine = null;
+            }
+            ColorRoutine = PortraitColorChange(Color.white, fadeDuration);
+            StartCoroutine(ColorRoutine);
+        }
+
+        if (character.portrait != null)
+            portrait.sprite = character.portrait;
+
         nameText.text = character.name;
+        lineText.color = character.setColor;
+        textbox.color = character.setColor;
+        continueIndicator.color = character.setColor;
         currentCharacter = character;
+    }
+
+    IEnumerator PortraitColorChange(Color endColor, float speed)
+    {
+        float timer = 0;
+        Color startColor = portrait.color;
+        while (timer < fadeDuration)
+        {
+            timer += Time.unscaledDeltaTime;
+            portrait.color = Color.Lerp(startColor, endColor, timer / fadeDuration);
+            yield return null;
+        }
+        portrait.color = endColor;
+
+        ColorRoutine = null;
     }
 }
 
@@ -182,4 +237,6 @@ public class Character
     public Sprite portrait;
     public string name;
     public string nameID;
+    public Color setColor;
+    public TMP_FontAsset font;
 }
