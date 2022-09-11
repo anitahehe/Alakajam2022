8f916e8320c491cbef087348eea9ad2f8b3ed20d
Harrison Kerr <harrison@builtbysnowman.com>
2022-09-11 14:36:41 -0400

    compass

--- Alakajam2022/Assets/Scripts/Compass.cs
+++ Alakajam2022/Assets/Scripts/Compass.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Compass : MonoBehaviour
 {
+    public Transform target;
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,15 @@ public class Compass : MonoBehaviour
     void Update()
     {
         
+    }
+    private void LateUpdate()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,rotation,speed *Time.deltaTime);
+       
     }
 }
