1e51d92c9bfaaed18204ae85802a845bb217637c
fignition <fignition@proton.me>
2022-09-15 19:31:35 -0400

    Add logic for compass, fix game progression, and fix collider shape on Skull island to match new sprite.

--- Alakajam2022/Assets/Scripts/Compass.cs
+++ Alakajam2022/Assets/Scripts/Compass.cs
@@ -7,10 +7,19 @@ public class Compass : MonoBehaviour
     public Transform target;
     public float speed = 5f;
 
+    public enum ISLAND { HELM, NAVI, SKUL }
+    public ISLAND island;
+
+    private SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponentInChildren<SpriteRenderer>();
+        if (island == ISLAND.SKUL)
+        {
+            sr.enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -18,14 +27,28 @@ public class Compass : MonoBehaviour
     {
         
     }
+
+
     private void LateUpdate()
     {
+        // Direction
         Vector2 direction = target.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-
         transform.rotation = Quaternion.Slerp(transform.rotation,rotation,speed *Time.deltaTime);
-       
+
+        // Appearance
+        switch (island)
+        {
+        case ISLAND.HELM:
+            sr.enabled = !GameManager.Instance().hasShipwrightItem;
+            break;
+        case ISLAND.NAVI:
+            sr.enabled = !GameManager.Instance().hasNavigatorItem;
+            break;
+        case ISLAND.SKUL:
+            sr.enabled = GameManager.Instance().hasNavigatorItem && GameManager.Instance().hasShipwrightItem;
+            break;
+        }
     }
 }
